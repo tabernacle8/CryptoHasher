612ffadd9442dc2aa09d2f9ed98f7878f2370366
Carter Kosturos <tabernacle800@gmail.com>
2023-06-07 15:07:34 -0400

    Small message

--- Run CryptoHasher.bat
+++ Run CryptoHasher.bat
@@ -3,6 +3,7 @@
 ::  This program executes a local java file to create crypto hashes for verification
 
 @echo Opening Crypto Hasher...
+@echo If you see no buttons, try expanding the window.
 @echo off
 cd %CD%
 java -jar FinishedProject.jar
